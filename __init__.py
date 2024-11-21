5aabb02ee8bf5c05f8a9d677390e7d0f3843197b
Juheon Chu <juhuhni98@gmail.com>
2024-11-21 02:50:09 -0500

    Organized GitHub repository by deleting redundant files and adding .gitignore file & folders.

--- __init__.py
+++
@@ -1,9 +0,0 @@
-~/ppi-evaluation/
-├── util.py
-├── data/                    
-├── analysis/
-│   ├── __init__.py          
-│   ├── af3_analysis/
-│   │   ├── __init__.py
-│   │   ├── run_af3.py     
-
